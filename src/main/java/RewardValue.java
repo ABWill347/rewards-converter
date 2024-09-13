5a91be9697634b7cf0bce639d975de68a86b4da2
ABWill347 <awilson@wil.yearup.org>
2024-09-13 16:20:08 -0400

    Update

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -2,17 +2,18 @@ public class RewardValue {
 
     private double cashValue;
     private int milesValue;
+    private static final double conversionRate = 0.0035;
 
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = (int) (cashValue / 0.0035);
+        this.milesValue = (int) (cashValue / conversionRate);
     }
 
 
     public RewardValue(int milesValue) {
         this.milesValue = milesValue;
-        this.cashValue = milesValue * 0.0035;
+        this.cashValue = milesValue * conversionRate;
     }
 
 
@@ -20,7 +21,7 @@ public class RewardValue {
         return cashValue;
     }
 
-    // Returns the miles value
+
     public int getMilesValue() {
         return milesValue;
     }
